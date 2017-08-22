d9fd3678e8a34039f1bdbc7e0c03d80b60e85a05
Dmytro Zghoba <dmytro.zghoba@digitalkozak.com>
2017-08-22 18:00:55 +0300

    bugfix: selected value disappears

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import suggestions from '../mocks/suggestions.json'
 
 export function getSuggestions(value) {
+  const lowerCasedValue = value.toLowerCase()
   return new Promise((resolve) => {
     setTimeout(() => {
       const rv = suggestions
-        .filter(a => a.name.includes(value))
+        .filter(a => a.name.toLowerCase().includes(lowerCasedValue))
         .map(({ name, code }) => ({ label: name, value: code }))
 
       return resolve(rv)
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,12 @@
+import _ from 'lodash'
 import { combineReducers } from 'redux'
 import { handleActions } from 'redux-actions'
 
 const suggestions = handleActions({
-  FETCH_SUGGESTIONS_REQUEST: (state) => ({ ...state, isLoading: true }),
-  FETCH_SUGGESTIONS_SUCCESS: (state, { payload: { data = [] } }) => ({ ...state, isLoading: false, data }),
+  FETCH_SUGGESTIONS_REQUEST: (state) => ({ ...state, isLoading: true, error: null }),
+  FETCH_SUGGESTIONS_SUCCESS: (state, { payload: { data = [] } }) => ({
+    ...state, data: _.unionBy(state.data, data, 'value'), isLoading: false
+  }),
   FETCH_SUGGESTIONS_FAILURE: (state, { payload: { error } }) => ({ ...state, isLoading: false, error }),
 }, {
   data: [],
